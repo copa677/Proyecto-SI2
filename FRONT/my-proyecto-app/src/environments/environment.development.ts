59abf0a3a618165c747a233c874f782cd86dfb4e
copa777 <copadalessandro@gmail.com>
2025-09-15 16:17:49 -0400

    modificaciones

--- FRONT/my-proyecto-app/src/environments/environment.development.ts
+++ FRONT/my-proyecto-app/src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    endpoint: 'ec2-3-15-162-42.us-east-2.compute.amazonaws.com:8000/',
+    endpoint: 'ec2-13-59-96-168.us-east-2.compute.amazonaws.com:8000/',
     //endpoint: 'http://127.0.0.1:8000/'
 };
