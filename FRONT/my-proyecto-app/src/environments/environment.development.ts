f6b530690340dba2066b7712961e8050b0cc2d7e
copa777 <copadalessandro@gmail.com>
2025-09-15 15:23:07 -0400

    actualizar las env

--- FRONT/my-proyecto-app/src/environments/environment.development.ts
+++ FRONT/my-proyecto-app/src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    //endpoint: 'http://ec2-3-133-150-252.us-east-2.compute.amazonaws.com:8000/',
-    endpoint: 'http://127.0.0.1:8000/'
+    endpoint: 'ec2-3-15-162-42.us-east-2.compute.amazonaws.com:8000/',
+    //endpoint: 'http://127.0.0.1:8000/'
 };
