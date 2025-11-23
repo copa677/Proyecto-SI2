dddd4f6be0d2032f3f8735d8efbe3b67f1e9b90b
copa777 <copadalessandro@gmail.com>
2025-11-23 16:42:02 -0400

    actualizacion para produccion

--- FRONT/my-proyecto-app/src/environments/environment.development.ts
+++ FRONT/my-proyecto-app/src/environments/environment.development.ts
@@ -1,5 +1,4 @@
 export const environment = {
-    endpoint: 'http://ec2-18-116-35-65.us-east-2.compute.amazonaws.com:8000/',
+    endpoint: 'ec2-3-134-106-25.us-east-2.compute.amazonaws.com:8000/',
     //endpoint: 'http://localhost:8000/'
-    
 };
